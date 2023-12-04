4edffa60ba7966aaa53a30b8b108f82f7d36859d
kasama03 <kasamal0310@gmail.com>
2023-12-04 17:21:34 +0700

    course

--- frontend/src/pages/courseSetting.tsx
+++ frontend/src/pages/courseSetting.tsx
@@ -1,42 +1,46 @@
-// import React from "react";
-// import {
-//   Table,
-//   TableBody,
-//   TableCaption,
-//   TableCell,
-//   TableFooter,
-//   TableHead,
-//   TableHeader,
-//   TableRow,
-// } from "@shadcn/ui/table"
+import courseimg from "../assets/coursesettingBG.jpg";
+import { Label } from "@shadcn/ui";
+import Form from "@shadcn/simplify/form";
+import { Input } from "@shadcn/ui/input";
 
-// const CourseSetting = () => {
-//   const backgroundImageUrl = "";
-//   return (
-//     <div className="bg-secondary w-full h-screen flex items-center justify-center">
-//       {/* <div className="h-full w-full bg-cover bg-center"
-//         style={{ backgroundImage: `url(${backgroundImageUrl})` }}> */}
-//         <h1 className="text-black text-xl font-mono absolute bg-white px-6 py-2 rounded-lg top-20 left-10">Schedule - Course Setting</h1>
-//         <Table>
-//           <TableHeader>
-//           <TableRow>
-//             <TableHead className="w-[100px]">Invoice</TableHead>
-//             <TableHead>Status</TableHead>
-//             <TableHead>Method</TableHead>
-//             <TableHead className="text-right">Amount</TableHead>
-//           </TableRow>
-//           </TableHeader>
-//         </Table>
-//       {/* </div> */}
-//     </div>
-//   );
-// };
-
-// export default CourseSetting;
-import React from "react";
 
 const CourseSetting = () => {
-  return <div>courseSetting</div>;
+  return (
+    <div className="grid md:grid-cols-2 w-full h-full">
+        <section className="w-full h-full p-2 relative">
+          <img
+            src={courseimg}
+            className="w-full h-full object-cover rounded "
+          />
+        </section>
+        <section className="h-full w-full flex justify-center items-center relative">
+            <div className=" w-full h-full p-20 max-w-lg flex justify-center flex-col">
+                <Label className=" text-4xl font-bold text-center">
+                    Course Setting
+                </Label>
+                <form
+                  className="flex flex-col gap-4 mt-12">
+                    <Label>
+                      Course Name<span className="text-red-500">*</span>
+                    </Label>
+                    <Input></Input>
+                    <Label>
+                      Participants<span className="text-red-500">*</span>
+                    </Label>
+                    <Input></Input>
+                    <Label>
+                      Experience<span className="text-red-500">*</span>
+                    </Label>
+                    <Input></Input>
+                    <Label>
+                      Description<span className="text-red-500">*</span>
+                    </Label>
+                    <Input></Input>
+                </form>
+            </div>
+        </section>
+      </div>
+  );
 };
 
 export default CourseSetting;
