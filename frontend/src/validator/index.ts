cd838b26c7c11994355a08a5770a388818d6364c
chadawan9913 <b6409913@g.sut.ac.th>
2023-12-22 17:47:58 +0700

    update validate Employee Health

--- frontend/src/validator/index.ts
+++ frontend/src/validator/index.ts
@@ -25,3 +25,46 @@ export const courseFormSchema = z.object({
 });
 
 export type CourseFormData = z.infer<typeof courseFormSchema>
+
+export const employeeFormSchema = z.object({
+  FirstName: z.string().min(2, "FirstName must be at least 2 characters"),
+  LastName: z.string().min(2, "Tooth must be at least 2 characters"),
+  Email: z.string().email("Please enter a valid email"),
+  Phone: z.string().max(10, "Phone must be at least 10 characters"),
+  Password: z
+    .string()
+    .min(2, "Password must be at least 8 characters long")
+    .max(20, "Password must be at most 20 characters long"),
+  DayOfBirth: z.date().max(new Date(), "Date must be in the past"),
+  PositionID: z.number({ required_error: "Please select a position" }),
+  PrecedeID: z.number({ required_error: "Please select a precede" }),
+  GenderID: z.number({ required_error: "Please select a gender" }),
+});
+
+export type EmployeeFormData = z.infer<typeof employeeFormSchema>
+
+export const employeeupdateFormSchema = z.object({
+  FirstName: z.string().min(2, "FirstName must be at least 2 characters"),
+  LastName: z.string().min(2, "Tooth must be at least 2 characters"),
+  Email: z.string().email("Please enter a valid email"),
+  Phone: z.string().max(10, "Phone must be at least 10 characters"),
+  DayOfBirth: z.date().max(new Date(), "Date must be in the past"),
+  PositionID: z.number({ required_error: "Please select a position" }),
+  PrecedeID: z.number({ required_error: "Please select a precede" }),
+  GenderID: z.number({ required_error: "Please select a gender" }),
+});
+
+export type EmployeeupdateFormData = z.infer<typeof employeeupdateFormSchema>
+
+export const healthFormSchema = z.object({
+  Vital: z.string().min(4, "Vital must be at least 4 characters"),
+    Tooth: z.string().min(4, "Tooth must be at least 4 characters"),
+    Vaccine: z.string().min(4, "Vaccine must be at least 4 characters"),
+    Parasite: z.string().min(4, "Parasite must be at least 4 characters"),
+    Blood: z.string().min(4, "Blood must be at least 4 characters"),
+    Date: z.date().min(new Date(), "Date must be in the future"),
+    HorseID: z.number({ required_error: "Please select a horse" }),
+    EmployeeID: z.number({ required_error: "Please select a employee" }),
+});
+
+export type HealthFormData = z.infer<typeof healthFormSchema>
