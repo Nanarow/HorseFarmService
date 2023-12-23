4ca513db6a9d7bbddab25f44eaef6abf952eedd6
Woranan Sholee <woranan.sholee@gmail.com>
2023-12-23 14:21:53 +0700

    ✅ Adding Tests: integration, unit

--- backend/tests/integration/tour_test.go
+++ backend/tests/integration/tour_test.go
@@ -14,10 +14,9 @@ import (
 	"github.com/sut66/team16/backend/routers"
 )
 
-// Successfully create a new tour registration with valid data
 func TestCreateTour(t *testing.T) {
+	entity.SetupDatabase("TestDB")
 	t.Run(`created tour success`, func(t *testing.T) {
-		entity.SetupDatabase()
 		router := routers.SetUpRouter()
 		router.POST("/tours", controllers.CreateTour)
 
@@ -41,5 +40,22 @@ func TestCreateTour(t *testing.T) {
 		// Assert
 		assert.Equal(t, http.StatusCreated, response.Code)
 		// add additional assertions to check if the tour registration is created successfully
+	})
+	t.Run(`create tour fail`, func(t *testing.T) {
+		router := routers.SetUpRouter()
+		router.POST("/tours", controllers.CreateTour)
+
+		tour := entity.TourRegistration{
+			Email: "u1@u.com",
+			Name:  "my tour",
+		}
+		tourJSON, _ := json.Marshal(tour)
+		request, _ := http.NewRequest("POST", "/tours", bytes.NewBuffer(tourJSON))
+		request.Header.Set("Content-Type", "application/json")
+
+		response := httptest.NewRecorder()
+		router.ServeHTTP(response, request)
+
+		assert.Equal(t, http.StatusBadRequest, response.Code)
 	})
 }
