a5eb8b752cae3a6854bd384a859b7cfa1874fb88
Woranan Sholee <woranan.sholee@gmail.com>
2023-12-26 22:14:13 +0700

    ⚡ General Update:  AuthProvider

--- frontend/src/providers/authProvider.tsx
+++ frontend/src/providers/authProvider.tsx
@@ -1,13 +1,20 @@
 import { Employee, User } from "@src/interfaces";
 import { http } from "@src/services/httpRequest";
 import { createContext, PropsWithChildren, useContext, useState } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
 
 type Role = "user" | "employee" | "admin";
+type TLogin = {
+  Email: string;
+  Password: string;
+};
+
 type AuthContextProps = {
-  user: User | undefined;
-  setUser: React.Dispatch<React.SetStateAction<User | undefined>>;
-  employee: Employee | undefined;
-  setEmployee: React.Dispatch<React.SetStateAction<Employee | undefined>>;
+  isLoggedIn: () => boolean;
+  getRole: () => Role;
+  getUser: () => User;
+  getEmployee: () => Employee;
+  handleLogin: (role: Role, data?: TLogin) => Promise<void>;
   logout: () => Promise<void>;
 };
 
@@ -22,30 +29,80 @@ function useAuth() {
 const AuthProvider = ({ children }: PropsWithChildren) => {
   const [user, setUser] = useState<User>();
   const [employee, setEmployee] = useState<Employee>();
-  const postLogout = async (role: Role) => {
-    console.log("logout as ", role);
+  const navigate = useNavigate();
+  const location = useLocation();
+  const from = location.state?.from || "/home";
+
+  const logout = async () => {
+    const role = getRole();
     const res = await http.Post("/logout/" + role, {});
     if (res.ok) {
       setEmployee(undefined);
       setUser(undefined);
     }
   };
-  const logout = async () => {
+
+  const getUser = () => {
+    if (!user) {
+      throw new Error("User not found");
+    }
+    return user;
+  };
+
+  const getEmployee = () => {
+    if (!employee) {
+      throw new Error("Employee not found");
+    }
+    return employee;
+  };
+
+  const isLoggedIn = () => {
     if (user) {
-      if (user.RoleID === 101) {
-        await postLogout("user");
-      } else {
-        await postLogout("admin");
-      }
+      return true;
     }
     if (employee) {
-      await postLogout("employee");
+      return true;
+    }
+    return false;
+  };
+
+  const getRole = () => {
+    if (employee) {
+      return "employee";
+    }
+    if (!user || (user && user.RoleID === 101)) {
+      return "user";
+    }
+    return "admin";
+  };
+
+  const handleLogin = async (role: Role, data?: TLogin) => {
+    if (role === "user" || role === "admin") {
+      const org = role === "user" ? "/login" : "/login/admin";
+      const res = await http.Post<User>(org, data || {});
+      if (res.ok) {
+        setUser(res.data);
+        navigate(from, { replace: true });
+      }
+    } else if (role === "employee") {
+      const res = await http.Post<Employee>("/login/employee", data || {});
+      if (res.ok) {
+        setEmployee(res.data);
+        navigate(from, { replace: true });
+      }
     }
   };
 
   return (
     <AuthContext.Provider
-      value={{ user, setUser, employee, setEmployee, logout }}
+      value={{
+        isLoggedIn,
+        getRole,
+        getUser,
+        getEmployee,
+        handleLogin,
+        logout,
+      }}
     >
       {children}
     </AuthContext.Provider>
