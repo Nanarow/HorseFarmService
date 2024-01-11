1424fe209077f6d656498cf9e707cae2c1b0fed3
Woranan Sholee <woranan.sholee@gmail.com>
2024-01-12 00:09:56 +0700

    ⚡ General Update:  Add tailwind plugin

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,3 @@
-import "./App.css";
 import { Route, Routes } from "react-router-dom";
 import {
   Course,
@@ -26,35 +25,36 @@ import StablePage from "./pages/stable";
 function App() {
   return (
     <Routes>
-      <Route path="/*" element={<NoPage />}></Route>
-      <Route path="/" element={<Home />}></Route>
-      <Route path="/loading/:page" element={<Loading />}></Route>
-      <Route path="/login/admin" element={<Login role="admin" />}></Route>
-      <Route path="/login/employee" element={<Login role="employee" />}></Route>
-      <Route path="/login" element={<Login role="user" />}></Route>
+      <Route path="/*" element={<NoPage />} />
+      <Route path="/" element={<Home />} />
+      <Route path="/loading/:page" element={<Loading />} />
+      <Route path="/login/admin" element={<Login role="admin" />} />
+      <Route path="/login/employee" element={<Login role="employee" />} />
+      <Route path="/login" element={<Login role="user" />} />
+
       <Route path="/example/form" element={<ValidateForm />} />
       <Route path="/example/table" element={<DragDropTable />} />
 
       <Route element={<PrivateRoute role={101} path="/login" />}>
-        <Route path="/course" element={<Course />}></Route>
-        <Route path="/tour" element={<Tour />}></Route>
+        <Route path="/course" element={<Course />} />
+        <Route path="/tour" element={<Tour />} />
       </Route>
 
       <Route element={<PrivateRoute role={100} path="/login/admin" />}>
-        <Route path="/employee" element={<Employee />}></Route>
-        <Route path="/employee/list" element={<EmployeeList />}></Route>
-        <Route path="/user" element={<User />}></Route>
-        <Route path="/user/list" element={<UserList />}></Route>
+        <Route path="/employee" element={<Employee />} />
+        <Route path="/employee/list" element={<EmployeeList />} />
+        <Route path="/user" element={<User />} />
+        <Route path="/user/list" element={<UserList />} />
       </Route>
 
       <Route element={<PrivateRoute position={201} path="/login/employee" />}>
-        <Route path="/course/setting" element={<CourseSetting />}></Route>
-        <Route path="/food" element={<Food />}></Route>
-        <Route path="/health" element={<Health />}></Route>
-        <Route path="/stable" element={<Stable />}></Route>
-        <Route path="/stable/list" element={<StableList />}></Route>
-        <Route path="/stable" element={<StablePage />}></Route>
-        <Route path="/horse" element={<Horse />}></Route>
+        <Route path="/course/setting" element={<CourseSetting />} />
+        <Route path="/food" element={<Food />} />
+        <Route path="/health" element={<Health />} />
+        <Route path="/stable" element={<Stable />} />
+        <Route path="/stable/list" element={<StableList />} />
+        <Route path="/stable" element={<StablePage />} />
+        <Route path="/horse" element={<Horse />} />
       </Route>
     </Routes>
   );
